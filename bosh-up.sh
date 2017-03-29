da4feb76aaaf7d9a57b554e42dabe808ccf34b5f
Micah Young <myoung@pivotal.io>
2017-03-29 10:55:00 -0400

    Fix wrapper name
    Refactor to allow more content

--- bosh-up.sh
+++ bosh-up.sh
@@ -7,11 +7,13 @@ else
   network_uuid=${1:?"Net UUID required"}
 
 cat > bosh-uvt.sh <<EOF
-echo $(tar cJ bosh-vm.sh | base64 -w0) | base64 -d | tar xJ > bosh-vm.sh
+cat << EOF2 | base64 -d  | tar xJ > bosh-vm.sh
+$(tar cJ bosh-vm.sh | base64 -w0)
+EOF2
 ./bosh-vm.sh $(echo $network_uuid)
 EOF
 
-  uvt-kvm create --cpu=1 --memory=1024 --disk=10 --run-script-once=bosh-vm-wrapper.sh $dom_name release=xenial arch=amd64
+  uvt-kvm create --cpu=1 --memory=1024 --disk=10 --run-script-once=bosh-uvt.sh $dom_name release=xenial arch=amd64
 fi
 
 while ! uvt-kvm ip $dom_name; do sleep 1; done
